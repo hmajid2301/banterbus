bb1822602780145f4815a7c8e9be03ebc52ccff4
Haseeb Majid <hello@haseebmajid.dev>
2025-01-06 11:57:48 +0000

    fix: crash loop pods missing cacerts

--- containers/service.nix
+++ containers/service.nix
@@ -8,16 +8,20 @@ pkgs.dockerTools.buildImage {
   created = "now";
   copyToRoot = pkgs.buildEnv {
     name = "image-root";
-    paths = [package];
+    paths = [
+      package
+      pkgs.cacert
+    ];
     pathsToLink = ["/bin"];
   };
   config = {
     ExposedPorts = {
       "8080/tcp" = {};
     };
-    Env = [
-      "BANTERBUS_DB_FOLDER=/"
-    ];
     Cmd = ["${package}/bin/banterbus"];
+    Env = [
+      "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      "SSL_CERT_DIR=${pkgs.cacert}/etc/ssl/certs/"
+    ];
   };
 }
