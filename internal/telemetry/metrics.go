ca389e7896a91e08e70a35e8a4f3a66f0b8f0f80
Haseeb Majid <hello@haseebmajid.dev>
2025-01-28 23:24:00 +0000

    feat: [sc-358] Add metrics

--- internal/telemetry/metrics.go
+++ internal/telemetry/metrics.go
@@ -2,16 +2,102 @@ package telemetry
 
 import (
 	"context"
+	"sync/atomic"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 )
 
+var activeConnectionsCount int64
+
+func IncrementSubscribers(ctx context.Context) error {
+	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
+
+	counter, err := m.Int64Counter("websocket_total_connections",
+		metric.WithDescription("Total number of subscribers to the websocket."),
+		metric.WithUnit("{call}"),
+	)
+	if err != nil {
+		return err
+	}
+
+	counter.Add(ctx, 1)
+	return nil
+}
+
+func ActiveConnections(_ context.Context) error {
+	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
+
+	gauge, err := m.Int64ObservableGauge("websocket.active_connections",
+		metric.WithDescription("Total number of active websocket connections."),
+		metric.WithUnit("1"),
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.RegisterCallback(
+		func(_ context.Context, o metric.Observer) error {
+			o.ObserveInt64(gauge, atomic.LoadInt64(&activeConnectionsCount))
+			return nil
+		},
+		gauge,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func IncrementActiveConnections() {
+	atomic.AddInt64(&activeConnectionsCount, 1)
+}
+
+func DecrementActiveConnections() {
+	atomic.AddInt64(&activeConnectionsCount, -1)
+}
+
+func RecordConnectionDuration(ctx context.Context, time float64) error {
+	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
+
+	histogram, err := m.Float64Histogram("websocket.connection_duration",
+		metric.WithDescription("Time taken for a subscription to be closed."),
+		metric.WithUnit("s"),
+	)
+	if err != nil {
+		return err
+	}
+
+	histogram.Record(ctx, time)
+	return nil
+}
+
+func RecordRequestLatency(ctx context.Context, latency float64, messageType string, status string) error {
+	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
+
+	histogram, err := m.Float64Histogram("handle_message.latency",
+		metric.WithDescription("Time taken to handle message from client."),
+		metric.WithUnit("ms"),
+		metric.WithExplicitBucketBoundaries([]float64{0.1, 0.5, 1, 2, 5}...),
+	)
+	if err != nil {
+		return err
+	}
+
+	histogram.Record(
+		ctx,
+		latency,
+		metric.WithAttributes(attribute.String("message_type", messageType), attribute.String("status", status)),
+	)
+	return nil
+}
+
 func IncrementMessageReceived(ctx context.Context, messageType string) error {
 	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
 
-	counter, err := m.Int64Counter("message_type_count",
+	counter, err := m.Int64Counter("total_messages_received",
 		metric.WithDescription("Total number of messages received."),
 		metric.WithUnit("{call}"),
 	)
@@ -26,7 +112,7 @@ func IncrementMessageReceived(ctx context.Context, messageType string) error {
 func IncrementMessageReceivedError(ctx context.Context) error {
 	m := otel.Meter("gitlab.com/hmajid2301/banterbus")
 
-	counter, err := m.Int64Counter("message_type_error_count",
+	counter, err := m.Int64Counter("total_message_errors",
 		metric.WithDescription("Total number of messages received which throw an error."),
 		metric.WithUnit("{call}"),
 	)
